645940353b15bcd95fc0b964a936ef01a1b1e8aa
JuanDiegoAvila <juanavila20027@gmail.com>
2023-04-26 11:55:58 -0600

    Frozen Lake funcional
    Co-authored-by: Manuel-Archila <Manuel-Archila@users.noreply.github.com>
    Co-authored-by: Diego Franco <Franco-611@users.noreply.github.com>

--- FrozenLake.py
+++ FrozenLake.py
@@ -2,8 +2,8 @@ import gymnasium as gym
 from gymnasium.envs.toy_text.frozen_lake import generate_random_map
 import numpy as np
 
-
-env = gym.make("FrozenLake-v1", desc=None, map_name="4x4", is_slippery=True, render_mode="human")
+custom_map = generate_random_map(size=3)
+env = gym.make("FrozenLake-v1", desc=custom_map, is_slippery=True, render_mode="human")
 
 # ACTION SPACE 0: left, 1: down, 2: right, 3: up
 # OBSERVATION SPACE 16: 4x4 grid
@@ -13,13 +13,11 @@ env = gym.make("FrozenLake-v1", desc=None, map_name="4x4", is_slippery=True, ren
 # 1. El agente llega al estado (max(nrow) * max(ncol) - 1) o [max(nrow)-1, max(ncol)-1](goal)
 # 2. El agente cae en un agujero (hole)
 
-# Q = [[0 for i in range(env.action_space.n)] for j in range(env.observation_space.n)]
-# Q = np.array(Q, dtype=np.float32)
 Q = np.zeros([env.observation_space.n, env.action_space.n])
 
 alpha = 0.8
 gamma = 0.95
-episodios = 1000
+episodios = 30
 
 for i in range(episodios):
     estado = env.reset()
@@ -28,7 +26,7 @@ for i in range(episodios):
     done = False
 
     while not done:
-        # Convertir el estado en un array unidimensional
+        
         estado_array = np.array([estado], dtype=np.int16)
 
         # Seleccionar una acción a partir del estado actual utilizando la política epsilon-greedy
@@ -42,10 +40,21 @@ for i in range(episodios):
 
         estado_array2 = np.array([estado2], dtype=np.int16)
 
-        Q[estado_array.item(), a] = Q[estado_array.item(), a] + alpha * (recompensa + gamma * np.max(Q[estado_array.item(), :]) - Q[estado_array.item(), a])
+        Q[estado_array.item(), a] = Q[estado_array.item(), a] + alpha * (recompensa + gamma * np.max(Q[estado_array2.item(), :]) - Q[estado_array.item(), a])
 
         estado = estado2
 
 print(Q)
 
 
+# se vuelve a ejecutar con los pasos optimos
+estado = env.reset()
+estado = estado[0]
+done = False
+while not done:
+    action = np.argmax(Q[int(estado)])
+    estado, _, done, _, _ = env.step(action)
+    env.render()
+
+# cerrar el entorno
+env.close()
