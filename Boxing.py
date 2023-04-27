dc237e3444fdbcf4b283bd6392878ff5989c68ed
Franco-611 <69815571+Franco-611@users.noreply.github.com>
2023-04-27 12:02:51 -0600

    intento
    Co-authored-by: JuanDiegoAvila <JuanDiegoAvila@users.noreply.github.com>
    Co-authored-by: Manuel-Archila <Manuel-Archila@users.noreply.github.com>

--- Boxing.py
+++ Boxing.py
@@ -4,7 +4,7 @@ import numpy as np
 # Crear el ambiente
 env = gym.make("ALE/Boxing-ram-v5")
 
-Q = np.zeros([env.action_space.n, env.observation_space.shape[0]])
+Q = np.zeros([env.observation_space.shape[0], env.action_space.n])
 
 # Establecemos los parámetros de aprendizaje
 alpha = 0.1
@@ -13,34 +13,38 @@ epsilon = 0.1
 num_episodios = 2000
 
 # Definimos una función auxiliar para convertir el estado en un array
-def estado_array(estado):
-    return np.array(estado).reshape((env.observation_space.shape[0],))
+def estado_array(estado_list):
+    estado_arr = np.array([np.array(i) for i in estado_list[0]])
+    return estado_arr.reshape(-1)
 
-# Comenzamos el entrenamiento
+
+# Comenzar el entrenamiento
 for episodio in range(num_episodios):
     estado = env.reset()
+    estado_idx = estado_array(estado)
     done = False
     
     while not done:
-        # Escogemos una acción e-greedy
+        # Escoger una acción e-greedy
         if np.random.uniform(0, 1) < epsilon:
             accion = env.action_space.sample()
         else:
-            accion = np.argmax(Q[:, estado_array(estado).item()])
+            accion = np.argmax(Q[tuple(estado_idx), :])
         
-        # Ejecutamos la acción y obtenemos la siguiente observación y la recompensa
+        # Ejecutar la acción y obtener la siguiente observación y la recompensa
         siguiente_estado, recompensa, done, info = env.step(accion)
+        siguiente_estado_idx = estado_array(siguiente_estado)
         
-        # Actualizamos la tabla Q
-        Q[accion, estado_array(estado).item()] += alpha * (recompensa + gamma * np.max(Q[:, estado_array(siguiente_estado).item()]) - Q[accion, estado_array(estado).item()])
+        # Actualizar la tabla Q
+        Q[tuple(estado_idx), accion] += alpha * (recompensa + gamma * np.max(Q[tuple(siguiente_estado_idx), :]) - Q[tuple(estado_idx), accion])
         
-        estado = siguiente_estado
+        estado_idx = siguiente_estado_idx
     
-    # Imprimimos el progreso
+    # Imprimir el progreso
     if (episodio+1) % 100 == 0:
         print("Episodio {} completado".format(episodio+1))
         
-#Indicar que termino
+# Indicar que termino
 print("Entrenamiento completado")
 
 # Evaluar el agente entrenado
@@ -51,7 +55,7 @@ total_reward = 0
 
 while not done:
     # Elegir la mejor acción
-    action = np.argmax(Q[int(estado)])
+    action = np.argmax(Q[:, int(estado)])
     
     # Realizar la acción y obtener la recompensa y el siguiente estado
     next_state, reward, done, info = env.step(action)
--- Boxing2.py
+++ Boxing2.py
@@ -1,60 +1,78 @@
 import gymnasium as gym
 import numpy as np
 
-# hiperparámetros
+# Crear el ambiente
+env = gym.make("ALE/Boxing-ram-v5")
+
+Q = np.zeros([env.action_space.n, env.observation_space.shape[0]])
+
+# Establecemos los parámetros de aprendizaje
 alpha = 0.1
-gamma = 0.99
-epsilon = 1.0
-epsilon_min = 0.01
-epsilon_decay = 0.999
+gamma = 0.6
+epsilon = 0.1
+num_episodios = 2000
 
-# crear entorno
-env = gym.make('ALE/Boxing-v5')
+# Definimos una función auxiliar para convertir el estado en un array
 
-# inicializar tabla Q
-state_space_size = env.observation_space.n
-action_space_size = env.action_space.n
-q_table = np.zeros((state_space_size, action_space_size))
+def estado_array(estado):
+    print(estado)
+    print('-------------------')
+    estado = np.array(estado).flatten()
+    estado_reshaped = estado.reshape(1, -1)
+    return np.ravel_multi_index(estado_reshaped[0], dims=env.observation_space.shape)
 
-# episodios
-num_episodes = 10000
-max_steps_per_episode = 10000
-total_reward = np.zeros(num_episodes)
 
-for episode in range(num_episodes):
-    state = env.reset()
+# Comenzamos el entrenamiento
+for episodio in range(num_episodios):
+    estado = env.reset()
+    # estado_idx = estado_array(estado)
+    estado_list = list(estado[0])
+    estado_list.append(estado[1]['lives'])  # Agrega el valor de 'lives' al final de la lista
+    estado_idx = np.ravel_multi_index(tuple(estado_reshaped[0]), dims=env.observation_space.shape)
+    print(estado)
     done = False
-    t = 0
-    episode_reward = 0
     
-    while not done and t < max_steps_per_episode:
-        # seleccionar acción mediante política epsilon-greedy
-        if np.random.uniform() < epsilon:
-            action = env.action_space.sample() # exploración
+    while not done:
+        # Escogemos una acción e-greedy
+        if np.random.uniform(0, 1) < epsilon:
+            accion = env.action_space.sample()
         else:
-            action = np.argmax(q_table[state, :]) # explotación
+            accion = np.argmax(Q[int(estado_idx), :])
         
-        # realizar acción y obtener observación y recompensa
-        next_state, reward, done, info = env.step(action)
+        # Ejecutamos la acción y obtenemos la siguiente observación y la recompensa
+        siguiente_estado, recompensa, done, info, prob = env.step(accion)
+        siguiente_estado_idx = estado_array(siguiente_estado)
         
-        # actualizar tabla Q
-        q_table[state, action] = q_table[state, action] + alpha * (reward + gamma * np.max(q_table[next_state, :]) - q_table[state, action])
+        # Actualizamos la tabla Q
+        Q[accion, estado_idx] += alpha * (recompensa + gamma * np.max(Q[:, siguiente_estado_idx]) - Q[accion, estado_idx])
         
-        # actualizar estado y recompensa acumulada
-        state = next_state
-        episode_reward += reward
-        t += 1
+        estado_idx = siguiente_estado_idx
     
-    # reducir valor de epsilon
-    epsilon = max(epsilon_min, epsilon * epsilon_decay)
-    
-    # almacenar recompensa del episodio
-    total_reward[episode] = episode_reward
-    
-    # imprimir progreso
-    if episode % 100 == 0:
-        avg_reward = np.mean(total_reward[max(0, episode-100):episode+1])
-        print(f'Episode {episode}/{num_episodes}, avg reward: {avg_reward:.2f}, epsilon: {epsilon:.2f}')
+    # Imprimimos el progreso
+    if (episodio+1) % 100 == 0:
+        print("Episodio {} completado".format(episodio+1))
+        
+#Indicar que termino
+print("Entrenamiento completado")
 
-# cerrar entorno
+# Evaluar el agente entrenado
+estado = env.reset()
+estado = estado[0]
+done = False
+total_reward = 0
+
+while not done:
+    # Elegir la mejor acción
+    action = np.argmax(Q[int(estado)])
+    
+    # Realizar la acción y obtener la recompensa y el siguiente estado
+    next_state, reward, done, info = env.step(action)
+    
+    # Actualizar el estado actual y la recompensa total
+    state = next_state
+    total_reward += reward
+    
+print("Total reward: {}".format(total_reward))
+
+# cerrar el entorno
 env.close()
