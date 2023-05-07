366b7f224e31d4203d863918f3f5927593e8a2cf
JuanDiegoAvila <juanavila20027@gmail.com>
2023-05-06 18:42:03 -0600

    algoritmo final

--- Boxing.py
+++ Boxing.py
@@ -1,70 +1,77 @@
 import gymnasium as gym
 import numpy as np
+import tensorflow as tf
 
-# Crear el ambiente
-env = gym.make("ALE/Boxing-ram-v5")
+# Crear el entorno del Boxeo
+env = gym.make("ALE/Boxing-ram-v5", difficulty=2)
 
-Q = np.zeros([env.observation_space.shape[0], env.action_space.n])
+# Crear una red neuronal con una capa oculta de 64 neuronas
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(64, activation='relu', input_shape=(env.observation_space.shape[0],)),
+    tf.keras.layers.Dense(env.action_space.n, activation='linear')
+])
 
-# Establecemos los parámetros de aprendizaje
-alpha = 0.1
-gamma = 0.6
-epsilon = 0.1
-num_episodios = 2000
+# Compilar el modelo con el optimizador Adam y la función de pérdida de error cuadrático medio
+model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001), loss='mse')
 
-# Definimos una función auxiliar para convertir el estado en un array
-def estado_array(estado_list):
-    estado_arr = np.array([np.array(i) for i in estado_list[0]])
-    return estado_arr.reshape(-1)
+# Entrenar el modelo
+episodes = 20
+steps = 200
+epsilon = 1.0
+epsilon_min = 0.01
+epsilon_decay = 0.999
+gamma = 0.89
 
+for episode in range(episodes):
+    state = env.reset()
+    state = np.reshape(state[0], [1, env.observation_space.shape[0]])
 
-# Comenzar el entrenamiento
-for episodio in range(num_episodios):
-    estado = env.reset()
-    estado_idx = estado_array(estado)
     done = False
+    total_reward = 0
     
-    while not done:
-        # Escoger una acción e-greedy
-        if np.random.uniform(0, 1) < epsilon:
-            accion = env.action_space.sample()
+    for step in range(steps):
+        # Elegir una acción con un valor epsilon-greedy
+        if np.random.rand() <= epsilon:
+            action = env.action_space.sample()
         else:
-            accion = np.argmax(Q[tuple(estado_idx), :])
+            action = np.argmax(model.predict(state, verbose=0)[0])
         
-        # Ejecutar la acción y obtener la siguiente observación y la recompensa
-        siguiente_estado, recompensa, done, info = env.step(accion)
-        siguiente_estado_idx = estado_array(siguiente_estado)
+        # Tomar la acción y obtener la siguiente observación, recompensa y estado de finalización
+        next_state, reward, done, _, _ = env.step(action)
+        next_state = np.reshape(next_state, [1, env.observation_space.shape[0]])
         
-        # Actualizar la tabla Q
-        Q[tuple(estado_idx), accion] += alpha * (recompensa + gamma * np.max(Q[tuple(siguiente_estado_idx), :]) - Q[tuple(estado_idx), accion])
+        # Calcular la recompensa total y ajustar la red neuronal
+        total_reward += reward
+        target = reward + gamma * np.amax(model.predict(next_state, verbose=0)[0])
+        target_f = model.predict(state, verbose=0)
+        target_f[0][action] = target
+        model.fit(state, target_f, epochs=1, verbose=0)
         
-        estado_idx = siguiente_estado_idx
-    
-    # Imprimir el progreso
-    if (episodio+1) % 100 == 0:
-        print("Episodio {} completado".format(episodio+1))
-        
-# Indicar que termino
-print("Entrenamiento completado")
+        # Actualizar el estado actual y la variable epsilon
+        state = next_state
+        if done:
+            print("Episodio {}, Recompensa total {}".format(episode, total_reward))
+            break
+    # Decrementar el valor de epsilon
+    if epsilon > epsilon_min:
+        epsilon *= epsilon_decay
 
-# Evaluar el agente entrenado
-estado = env.reset()
-estado = estado[0]
+env = gym.make("ALE/Boxing-ram-v5", render_mode="human")
+# correr un episodio
+state = env.reset()
+state = np.reshape(state[0], [1, env.observation_space.shape[0]])
+
 done = False
-total_reward = 0
-
 while not done:
-    # Elegir la mejor acción
-    action = np.argmax(Q[:, int(estado)])
+    # elegir una acción
+    action = np.argmax(model.predict(state, verbose = 0)[0])
     
-    # Realizar la acción y obtener la recompensa y el siguiente estado
-    next_state, reward, done, info = env.step(action)
+    # tomar la acción en el ambiente de juego
+    next_state, reward, done, info, _ = env.step(action)
+    next_state = np.reshape(next_state, [1, env.observation_space.shape[0]])
     
-    # Actualizar el estado actual y la recompensa total
+    # mostrar el ambiente de juego
+    env.render()
+    
+    # actualizar el estado actual
     state = next_state
-    total_reward += reward
-    
-print("Total reward: {}".format(total_reward))
-
-# cerrar el entorno
-env.close()
